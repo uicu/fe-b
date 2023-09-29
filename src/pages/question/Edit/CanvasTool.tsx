704e121499c692a250c19a97d3ad3e731494cda5
yangpan <738912168@qq..com>
2023-09-29 19:38:31 +0800

    feat(store): 新增

--- src/pages/question/Edit/CanvasTool.tsx
+++ src/pages/question/Edit/CanvasTool.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect, useState } from 'react'
 import { useDispatch } from 'react-redux'
+import { nanoid } from 'nanoid'
 import { Pagination, Divider, Dropdown, Button, message } from 'antd'
 import type { MenuProps } from 'antd'
 import { MoreOutlined } from '@ant-design/icons'
@@ -108,6 +109,109 @@ const CanvasTool: FC = () => {
       if (currentPage !== 1) {
         dispatch(changeCurrentPage(currentPage - 1))
       }
+    }
+
+    // 后移
+    if (e.key === '3') {
+      if (currentPage === pageTotal || currentPage === -1) return message.error('不允许后移')
+      const newComponent = componentList.map(item => {
+        if (item.page === currentPage) {
+          return {
+            ...item,
+            page: item.page + 1,
+          }
+        }
+        if (item.page === currentPage + 1) {
+          return {
+            ...item,
+            page: currentPage,
+          }
+        }
+        return {
+          ...item,
+        }
+      })
+      dispatch(replaceComponent(newComponent))
+
+      if (currentPage !== pageTotal) {
+        dispatch(changeCurrentPage(currentPage + 1))
+      }
+    }
+
+    // 复制
+    if (e.key === '4') {
+      // 找到当前页的所有组件
+      let currentComponent = componentList.filter(item => {
+        return item.page === currentPage
+      })
+      // 替换id
+      currentComponent = currentComponent.map(item => {
+        return {
+          ...item,
+          fe_id: nanoid(),
+          page: currentPage + 1,
+        }
+      })
+      // 找到要插入的index
+      const currentIndex = componentList.findIndex(item => {
+        return item.page === currentPage + 1
+      })
+      // 在当前位置之后的index往后移
+      const newComponent = componentList.map(item => {
+        if (item.page > currentPage) {
+          return {
+            ...item,
+            page: item.page + 1,
+          }
+        }
+        return {
+          ...item,
+        }
+      })
+      newComponent.splice(currentIndex, 0, ...currentComponent)
+      dispatch(replaceComponent(newComponent))
+      dispatch(changePageTotal(pageTotal + 1))
+      dispatch(changeCurrentPage(currentPage + 1))
+    }
+
+    // 新增
+    if (e.key === '5') {
+      // 当前要插入的足迹
+      const currentComponent = [
+        {
+          fe_id: nanoid(), // 前端生成的 id
+          type: 'questionInput', // 要和后端统一好
+          title: '单行输入',
+          page: currentPage + 1,
+          isHidden: false,
+          isLocked: false,
+          props: {
+            title: '输入框标题',
+            placeholder: '请输入...',
+          },
+        },
+      ]
+
+      // 找到要插入的index
+      const currentIndex = componentList.findIndex(item => {
+        return item.page === currentPage + 1
+      })
+      // 在当前位置之后的index往后移
+      const newComponent = componentList.map(item => {
+        if (item.page > currentPage) {
+          return {
+            ...item,
+            page: item.page + 1,
+          }
+        }
+        return {
+          ...item,
+        }
+      })
+      newComponent.splice(currentIndex, 0, ...currentComponent)
+      dispatch(replaceComponent(newComponent))
+      dispatch(changePageTotal(pageTotal + 1))
+      dispatch(changeCurrentPage(currentPage + 1))
     }
   }
 
